74aa50083add4cc89817763d26ad3d4a42cdd86b
Gideon Sireling <code@accursoft.com>
2014-10-06 15:05:04 +0300

    builder: move cabal files from /root to /
    debian sets HOME to /root, fedora set it to /

--- cabal.sh
+++ cabal.sh
@@ -8,12 +8,15 @@ cd cabal-install-*
 export EXTRA_CONFIGURE_OPTS=--ghc-option=-j
 sed -i "s|{CURL} -L|{CURL} -Ls|g" bootstrap.sh
 ./bootstrap.sh --no-doc
-mv /.cabal/bin/cabal /usr/local/bin
-
-#remove the packages installed by cabal-install, as they might not be the best ones to use when we install the frameworks
-rm -rf /cabal-install-* /.cabal /.ghc
+mv ~/.cabal/bin/cabal /usr/local/bin
 
 #generate default .cabal/config
 cabal get
+mkdir /.cabal
+sed "
 #replace hackage with stackage inclusive
-sed -i "s|remote-repo:.*|remote-repo: stackage:http://www.stackage.org/alias/fpcomplete/unstable-ghc78-inclusive|" /.cabal/config
+s|remote-repo:.*|remote-repo: stackage:http://www.stackage.org/alias/fpcomplete/unstable-ghc78-inclusive|
+s|/root||g" ~/.cabal/config >/.cabal/config
+
+#remove the packages installed by cabal-install and other cruft, as they might not be the best ones to use when we install the frameworks
+rm -rf /cabal-install-* ~
