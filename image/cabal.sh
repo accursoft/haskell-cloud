3deb991d47b56b712e40eff0e9e7632ae9327668
Gideon Sireling <code@accursoft.com>
2015-04-01 13:17:35 +0300

    builder: comment why we don't use bindir to install cabal-install

--- image/cabal.sh
+++ image/cabal.sh
@@ -7,6 +7,8 @@ cd cabal-install-*
 #build
 export EXTRA_CONFIGURE_OPTS="--ghc-option=-j"
 ./bootstrap.sh --no-doc --sandbox
+#we could use --bindir in EXTRA_CONFIGURE_OPTS, but bootstrap.sh would warn after successful installation
+#The 'cabal' executable was not successfully installed into .cabal-sandbox/bin/
 mv .cabal-sandbox/bin/cabal /usr/local/bin
 
 #remove the sandbox
